af8b92cdf8874eb4e75609729d01571ab75e98c2
aisahislam <124077995+aisahislam@users.noreply.github.com>
2023-02-01 08:01:17 +0800

    Add files via upload

---
+++ randomRestaurants.py
@@ -0,0 +1,10 @@
+from random import choice
+
+#list of restaurants
+restaurants = ['SCR','warung 2020', 'sugar bun', 'KFC', 'mcDonald', 'Sharing', 'Sushi Mentai', 'Pizza Hut', 'Aiman Mall Food Court', 'After 4 Kopitiam', 'Curry House', 
+               'Bombay Masala', 'Summer Mall Food Court', 'DudukSama Kopitiam', 'Sushi King', 'Dubuyo', 'Subway', 'Secret Recipe', 'The Spring Food Court', 'Fauziah Cafe', 
+               'Ceylonese', 'Himawari Ramen', 'The Chicken Rice Shop', 'Serapi Corner', 'Pelita Restaurant', 'Warung Kak Darot', 'Swee Kang Ais Kacang', 'Rojak Kucei', 
+               'Makan Sari Food Street', 'Nasi Kandar LineQ', 'Chicago7', 'PohonMas', 'UniHaus', 'Fendi Kelate Cafe', 'Dulang Kampung', 'The Food',
+               'Airis Thai', 'Tuk Tuk', 'Nirawan99', 'Unplugged Cafe']
+
+print(choice(restaurants))
